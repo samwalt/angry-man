57e06219f2a443fec66dca2cb48968557bbcd5ea
root <root@localhost.(none)>
2013-11-06 17:43:02 +0800

    新的rails项目

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AngryMan::Application.config.secret_key_base = '2b371ee122d98f69d3c3c56cb3f52e8ad437a261d825c9e89b7273fb437daed62bd517b7f3ab4cba1716f0ffbd2d356ace4b151ba3205775996cf621275a4406'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AngryMan::Application.config.session_store :cookie_store, key: '_angry-man_session'
