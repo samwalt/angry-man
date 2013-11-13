799574968297fe05c9fee78b45dcd7cb97be14a3
samwalt <samwalt@gmail.com>
2013-11-13 23:02:21 +0800

    create model Server

---
+++ db/migrate/20131113145556_create_servers.rb
@@ -0,0 +1,12 @@
+class CreateServers < ActiveRecord::Migration
+  def change
+    create_table :servers do |t|
+      t.string :code, :unique=>true, :limit=>2 # 服务的代号
+      t.string :service, :limit=>10  #Jira, Stash, Bamboo, Crucible
+      t.string :ip, :limit=>15
+      t.string :port, :limit=>5
+
+      t.timestamps
+    end
+  end
+end
