71da518de92744194ee678fdb0636f060c614af0
Steve <samwalt@gmail.com>
2013-11-18 20:09:15 +0800

    request from jira

--- app/controllers/webhook/jira_controller.rb
+++ app/controllers/webhook/jira_controller.rb
@@ -3,6 +3,18 @@ class Webhook::JiraController < ApplicationController
 
   def create
     @params = params
+	case @params[:status]
+	when 'InProgress'
+		# create feature branch in Stash
+	when 'Review'
+		# send email to developer to review code
+	when 'Reopen'
+		# send email to developer to fix bug
+	when 'Merge'
+		# merge and delete feature branch
+	else
+		# others
+	end
     render :nothing => true
   end
 
