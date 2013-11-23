2b1c4f94e7cb21945bb14f5c9ffd52eada577bd0
tao.yang <tao.yang@shuyun.com>
2013-11-23 14:23:45 +0800

    issue:整理辅助系统用到的api msg:没有

--- lib/atlassian/crucible/codeView.rb
+++ lib/atlassian/crucible/codeView.rb
@@ -1,6 +1,13 @@
 module Atlassian
   module Crucible
     module CodeView
+      def create_review
+        "/cru/rest-service/reviews-v1"
+      end
+
+      def add_changeset(reviewId)
+        "/cru/rest-service/reviews-v1/#{reviewId}/addChangeset"
+      end
 
     end
   end
--- lib/atlassian/stash/git.rb
+++ lib/atlassian/stash/git.rb
@@ -1,7 +1,25 @@
 module Atlassian
   module Stash
     module Git
+      # merge stash api
+      def merge
 
+      end
+
+      # create feature branch
+      def create_feature_branch
+
+      end
+
+      # create hotfix branch
+      def create_hotfix_branch
+
+      end
+
+      # create release branch
+      def create_release_branch
+
+      end
     end
   end
 end
