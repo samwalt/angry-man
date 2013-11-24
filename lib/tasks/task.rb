b10b2b8a3c634b1b05ddab4ac841b695c7042ca7
Steve <samwalt@gmail.com>
2013-11-24 16:59:05 +0800

    refactor event handler

--- lib/tasks/task.rb
+++ lib/tasks/task.rb
@@ -1,59 +1,70 @@
 require 'workflow'
 
 class Task
+
+	def push
+		puts "add to changeset"
+	end
+
+	def developer_submit
+		puts "review code"
+	end
+
+	def review_pass
+		puts "check every reviewer pass"
+	end
+
+	def review_not_pass
+		puts 'review not pass, to reopen'
+	end
+
+	def test_pass
+		puts "test pass, to merge"
+	end
+
+	def test_not_pass
+		puts 'test not pass, to reopen'
+	end
+
+	def merge_success
+		puts 'merge success, resolve'
+	end
+
+	def merge_failed
+		puts 'merge failed, to reopen'
+	end
+
 	include Workflow
 
 	workflow do
 		state :new do
-			puts "create feature branch"
-			event :push, :transitions_to => :new do
-				puts "add to changeset"
-			end
-			event :developer_submit, :transitions_to => :review do
-				puts "review code"
-			end
+			event :push, :transitions_to => :new
+			event :developer_submit, :transitions_to => :review
 		end
+
 		state :review do
-			event :review_pass, :transitions_to => :test do 
-				puts "check every reviewer pass"
-			end
-			event :review_not_pass, :transitions_to => :reopen do
-				puts 'review not pass, to reopen'
-			end
+			event :review_pass, :transitions_to => :test
+			event :review_not_pass, :transitions_to => :reopen
 		end
+
 		state :reopen do
-			puts "modify crucible state to reopen"
-			event :push, :transitions_to => :reopen do
-				puts "add to changeset"
-			end
-			event :developer_submit, :transitions_to => :review do
-				puts "review code"
-			end
-
+			event :push, :transitions_to => :reopen
+			event :developer_submit, :transitions_to => :review
 		end
+
 		state :test do
-			puts "modify jira state to stage"
-			event :test_pass, :transitions_to => :merge do
-				puts "test pass, to merge"
-			end
+			event :test_pass, :transitions_to => :merge
 
-			event :test_not_pass, :transitions_to => :reopen do
-				puts 'test not pass, to reopen'
-			end
-
+			event :test_not_pass, :transitions_to => :reopen
 		end
+
 		state :merge do
-			event :merge_success, :transitions_to => :resolve do
-				puts 'merge success, resolve'
-			end
-			event :merge_failed, :transitions_to => :reopen do
-				puts 'merge failed, to reopen'
-			end
+			event :merge_success, :transitions_to => :resolve
+			event :merge_failed, :transitions_to => :reopen
 		end
+
 		state :resolve do
 			puts "modify jira state to resolve"
-			
 		end
 	end
 end
-
