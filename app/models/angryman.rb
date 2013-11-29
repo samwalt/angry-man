48bd2616b790ea5b5d9a7a10ebca5895d6a419b5
samwalt <samwalt@gmail.com>
2013-11-29 22:58:04 +0800

    workflow代码移到model中

--- app/models/angryman.rb
+++ app/models/angryman.rb
@@ -1,3 +1,5 @@
+require 'workflow'
+
 class Angryman < ActiveRecord::Base
 	acts_as_avatarable
 
@@ -34,4 +36,39 @@ class Angryman < ActiveRecord::Base
 	def merge_failed
 		puts 'merge failed, to reopen'
 	end	
+
+
+	include Workflow
+
+	workflow do
+		state :new do
+			event :push, :transitions_to => :new
+			event :developer_submit, :transitions_to => :review
+		end
+
+		state :review do
+			event :review_pass, :transitions_to => :test
+			event :review_not_pass, :transitions_to => :reopen
+		end
+
+		state :reopen do
+			event :push, :transitions_to => :reopen
+			event :developer_submit, :transitions_to => :review
+		end
+
+		state :test do
+			event :test_pass, :transitions_to => :merge
+
+			event :test_not_pass, :transitions_to => :reopen
+		end
+
+		state :merge do
+			event :merge_success, :transitions_to => :resolve
+			event :merge_failed, :transitions_to => :reopen
+		end
+
+		state :resolve do
+			puts "modify jira state to resolve"
+		end
+	end
 end
